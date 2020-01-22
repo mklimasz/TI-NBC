32efb71bda601e54c7efce2f01da2205f604fbb1
Mateusz Klimaszewski <mk.klimaszewski@gmail.com>
2020-01-22 18:19:00 +0100

    Fix typo.

--- run_docker.sh
+++ run_docker.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 DIR=$(pwd)
-docker run -v "$DIR"/data/:/data nbc:latest bash -c "nbc --path data/s2.txt --k 10 -o /data/results.csv"
+docker run -v "$DIR"/data/:/data nbc:latest bash -c "nbc --path /data/s2.txt --k 10 -o /data/results.csv"
